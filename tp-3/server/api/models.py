524bbdcb02ad8edd5a2551e268d06edd2eb201f0
Ahmed Moualy Zein <72205123+Ahmed-mzn@users.noreply.github.com>
2022-01-09 21:41:13 +0000

    Update models.py

--- tp-3/server/api/models.py
+++ tp-3/server/api/models.py
@@ -6,6 +6,14 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     qty = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    
+    def get_created_at(self):
+        return self.created_at.strftime("%Y-%m-%d %H:%M:%S")
+    
+    def __str__(self):
+        return self.name
 
 
     class Meta:
